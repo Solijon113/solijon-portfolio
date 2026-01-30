db15305cf06dfff2d4fe7a0bf8161e6b5d7cc030
tbdotstagoloan-ship-it <tbdotstagoloan@gmail.com>
2026-01-30 10:26:57 +0800

    Fixed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.multiple-text', {
-      strings: ['Web Developer'],
+      strings: ['Web Developer', 'Laravel Developer'],
       typeSpeed: 80,
       backSpeed: 80,
       backDelay: 1200,
